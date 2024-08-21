ddd37f1228e0444fe32f422ba564e6201127b540
tomasmark79 <tomas@digitalspace.name>
2024-08-21 11:23:34 +0200

    renamed script to PowerSaver - more logical name renamed arguments and added short types for fasted and comfort use some output fixes and logicaly changes to achive desired goal added some comments for better understanding

--- powersaver.sh
+++ powersaver.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-echo "PowerMaster 2024.0821 Tomas Mark, usage:"
-echo -e "powermaster.sh [ \e[31mmax\e[0m | \e[33mmg\e[0m | \e[32mhalf\e[0m | \e[34multra\e[0m | \e[35mcustom [max_freq] [Mhz|GHz]\e[0m ]"
+appName="PowerSaver"
+version="2024.0821"
+author="Tomas Mark"
+email="tomas@digitalspace.name"
 
+settingMode="CPU Frequency Mode to"
+notifyIconPath="/home/tomas/Obrázky/Ikony/desktop-application.png"
 
+# help
+usage="powersaver.sh [ none | mg | half | ultra | custom [max_freq] [Mhz|GHz] ]"
+usageColored="powersaver.sh [ \e[31mnone\e[0m | \e[33mmg\e[0m | \e[32mhalf\e[0m | \e[34multra\e[0m | \e[35mcustom [max_freq] [Mhz|GHz]\e[0m ]"
 
-# Check cpu power package
-if ! dpkg -l | grep cpupower > /dev/null; then
+# dependencies
+if ! dpkg -l | grep cpupower >/dev/null; then
     echo -e "\e[31mcpupower package is not installed. Please install it first.\e[0m"
     echo -e "\e[31mRun: sudo apt install cpupower\e[0m"
     exit 1
 fi
 
-num_cores=$(nproc)
-echo
-echo -e "\e[35m$num_cores cpu cores detected $1\e[0m"
-
+# variables
 declare -a min_freqs
 declare -a min_freq_units
 declare -a max_freqs
@@ -26,15 +30,18 @@ declare -a min_freq_units_policy
 declare -a max_freqs_policy
 declare -a max_freq_units_policy
 
+required_mode=""
+
+# functions
 function convert_to_mhz {
     freq=$1
     unit=$2
     if [ "$unit" == "GHz" ]; then
-        
+
         # Tento prevod prevadi na MHz bez desetinnych hodnot
         # freq=$(echo "$freq" | awk -F. '{print $1}')
         # freq=$(( freq * 1000 ))
-        
+
         # Převod GHz na MHz s desetinnými hodnotami
         freq=$(echo "$freq * 1000" | bc -l)
     fi
@@ -43,63 +50,47 @@ function convert_to_mhz {
     # echo $freq
 }
 
-function max_mode {
-    echo
-    echo "Setting Max Mode ..."
-    echo "notice: maximal frequency will be set for all cores"
+function print_notify {
+    notify-send -u low -i $notifyIconPath "$appName" "Mode: $1"
+}
 
-    for ((i=0; i<$num_cores; i++)); do
-        echo "Setting high performance mode for core $i..."
+function none_mode {
+    for ((i = 0; i < $num_cores; i++)); do
+        echo -e -n "\e[33mForce to set core $i - min: ${min_freqs[$i]}${min_freq_units[$i]} - max: ${max_freqs[$i]}${max_freq_units[$i]}\e[0m "
         sudo cpupower -c $i frequency-set -d ${min_freqs[$i]}${min_freq_units[$i]} -u ${max_freqs[$i]}${max_freq_units[$i]}
     done
+    echo
 }
-
 function half_mode {
-    echo
-    echo "Setting Half Mode ..."
-    echo "notice: one half of maximal frequency will be set for all cores"
-   
-    for ((i=0; i<$num_cores; i++)); do
+    for ((i = 0; i < $num_cores; i++)); do
         max_freq=$(convert_to_mhz ${max_freqs[$i]} ${max_freq_units[$i]})
-        max_freq=$(( max_freq / 2 ))
+        max_freq=$((max_freq / 2))
         max_freq_units="MHz"
         echo -e -n "\e[33mAttempt to set core $i - min: ${min_freqs[$i]}${min_freq_units[$i]} - max: ${max_freq}${max_freq_units}\e[0m "
         sudo cpupower -c $i frequency-set -d ${min_freqs[$i]}${min_freq_units[$i]} -u ${max_freq}${max_freq_units}
     done
+    echo
 }
-
 function mg_mode {
-    echo
-    echo "Setting Minus Giga Mode ..."
-    echo "notice: maximal frequency MINUS 1Ghz will be set for all cores"
-   
-    for ((i=0; i<$num_cores; i++)); do
+    for ((i = 0; i < $num_cores; i++)); do
         max_freq=$(convert_to_mhz ${max_freqs[$i]} ${max_freq_units[$i]})
-        max_freq=$(( max_freq - 1000 ))
+        max_freq=$((max_freq - 1000))
         max_freq_units="MHz"
         echo -e -n "\e[33mAttempt to set core $i - min: ${min_freqs[$i]}${min_freq_units[$i]} - max: ${max_freq}${max_freq_units}\e[0m "
         sudo cpupower -c $i frequency-set -d ${min_freqs[$i]}${min_freq_units[$i]} -u ${max_freq}${max_freq_units}
     done
-}
-
-function ultra_mode {
     echo
-    echo "Setting Ultra Power Saver Mode ..."
-    echo "notice: minimal frequency will be set for all cores"
-
-    for ((i=0; i<$num_cores; i++)); do
+}
+function ultra_mode {
+    for ((i = 0; i < $num_cores; i++)); do
         echo -e -n "\e[33mAttempt to set core $i - min: ${min_freqs[$i]}${min_freq_units[$i]} - min: ${min_freqs[$i]}${min_freq_units[$i]}\e[0m "
         sudo cpupower -c $i frequency-set -d ${min_freqs[$i]}${min_freq_units[$i]} -u ${min_freqs[$i]}${min_freq_units[$i]}
     done
+    echo
 }
-
 # params $1 - max frequency, $2 - unit
 function custom_mode {
-    echo
-    echo "Setting Custom Power Saver Mode ..."
-    echo "notice: custom frequency will be set for all cores"
-
-    if [ -z "$1" ] || [ -z "$2" ]; then
+    if [[ -z "$1" || -z "$2" ]]; then
         echo -e "\e[31mMissing second or third param for max frequency and its unit.\e[0m"
         echo -e "\e[31mExample: ./powermaster.sh custom_power_saver 1.6 Ghz\e[0m - space char required between freq number and unit!"
         echo -e "\e[31mExample: ./powermaster.sh custom_power_saver 1600 Mhz\e[0m - space char required between freq number and unit!"
@@ -107,17 +98,16 @@ function custom_mode {
         return
     fi
 
-    for ((i=0; i<$num_cores; i++)); do
+    for ((i = 0; i < $num_cores; i++)); do
         echo -e -n "\e[33mAttempt to set core $i - min: ${min_freqs[$i]}${min_freq_units[$i]} - max: $1$2\e[0m "
         sudo cpupower -c $i frequency-set -d ${min_freqs[$i]}${min_freq_units[$i]} -u $1$2
     done
+    echo
 }
 
 function get_cpu_limits {
-    echo 
-    echo "Getting limits for all cores ..."
-
-    for ((i=0; i<$num_cores; i++)); do
+    echo "Getting physical processor frequency limits ..."
+    for ((i = 0; i < $num_cores; i++)); do
         cpu_info=$(sudo cpupower -c $i frequency-info)
         min_freqs[$i]=$(echo "$cpu_info" | grep "hardware limits:" | awk '{print $3}')
         min_freq_units[$i]=$(echo "$cpu_info" | grep "hardware limits:" | awk '{print $4}')
@@ -125,14 +115,13 @@ function get_cpu_limits {
         max_freq_units[$i]=$(echo "$cpu_info" | grep "hardware limits:" | awk '{print $7}')
         echo -e "\e[32mCore $i - min: ${min_freqs[$i]} ${min_freq_units[$i]} - max: ${max_freqs[$i]} ${max_freq_units[$i]}\e[0m"
     done
-    
+    echo
+
 }
 
 function get_cpu_policy {
-    echo
-    echo "Getting policy for all cores ..."
-
-    for ((i=0; i<$num_cores; i++)); do
+    echo "Getting physical processor frequency policies ..."
+    for ((i = 0; i < $num_cores; i++)); do
         cpu_info_policy=$(sudo cpupower -c $i frequency-info)
         min_freqs_policy[$i]=$(echo "$cpu_info_policy" | grep "policy" | awk '{print $7}')
         min_freq_units_policy[$i]=$(echo "$cpu_info_policy" | grep "policy" | awk '{print $8}')
@@ -140,31 +129,63 @@ function get_cpu_policy {
         max_freq_units_policy[$i]=$(echo "$cpu_info_policy" | grep "policy" | awk '{print $11}')
         echo -e "\e[34mCore $i - min: ${min_freqs_policy[$i]} ${min_freq_units_policy[$i]} - max: ${max_freqs_policy[$i]} ${max_freq_units_policy[$i]}\e[0m"
     done
+    echo
 }
 
-if [ "$1" == "max" ]; then
+function getCpuInfo {
+    echo "$appName $version $author"
+    echo
+    num_cores=$(nproc)
+    echo -e "Requested mode \e[41m$required_mode\e[0m for $num_cores detected cpu cores"
+    echo
+    cat /proc/cpuinfo | grep 'name' | uniq
+    echo
+}
+
+if [[ "$1" == "none" || "$1" == "n" ]]; then
+    required_mode="None"
+    getCpuInfo
     get_cpu_limits
-    max_mode
+    echo "$settingMode $required_mode"
+    print_notify "$required_mode"
+    none_mode
     get_cpu_policy
-elif [ "$1" == "half" ]; then
+elif [[ "$1" == "mg" || "$1" == "m" ]]; then
+    required_mode="Mg"
+    getCpuInfo
     get_cpu_limits
-    half_mode
-    get_cpu_policy
-elif [ "$1" == "ultra" ]; then
-    get_cpu_limits
-    ultra_mode
-    get_cpu_policy
-elif [ "$1" == "custom" ]; then
-    get_cpu_limits
-    custom_mode $2 $3
-    get_cpu_policy
-elif [ "$1" == "mg" ]; then
-    get_cpu_limits
+    echo "$settingMode $required_mode"
+    print_notify "$required_mode"
     mg_mode
     get_cpu_policy
+elif [[ "$1" == "half" || "$1" == "h" ]]; then
+    required_mode="Half"
+    getCpuInfo
+    get_cpu_limits
+    echo "$settingMode $required_mode"
+    print_notify "$required_mode"
+    half_mode
+    get_cpu_policy
+elif [[ "$1" == "ultra" || "$1" == "u" ]]; then
+    required_mode="Ultra"
+    getCpuInfo
+    get_cpu_limits
+    echo "$settingMode $required_mode"
+    print_notify "$required_mode"
+    ultra_mode
+    get_cpu_policy
+elif [[ "$1" == "custom" || "$1" == "c" ]]; then
+    required_mode="Custom"
+    getCpuInfo
+    get_cpu_limits
+    echo "$settingMode $required_mode $2 $3"
+    print_notify "$required_mode $2 $3"
+    custom_mode $2 $3
+    get_cpu_policy
 else
-    echo "No param was given, getting limits and policy for all cores ..."
+    required_mode="Only Print Informations"
+    echo -e $usageColored
+    getCpuInfo
     get_cpu_limits
     get_cpu_policy
 fi
-
